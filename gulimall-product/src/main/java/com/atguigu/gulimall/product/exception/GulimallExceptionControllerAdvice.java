075c039872639a02ec78014a220623688509a46d
lufeifeile <790002348@qq.com>
2021-09-22 17:16:58 +0800

    云存储

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/exception/GulimallExceptionControllerAdvice.java
@@ -0,0 +1,44 @@
+package com.atguigu.gulimall.product.exception;
+
+import com.atguigu.common.exception.BizCodeEnume;
+import com.atguigu.common.utils.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice(basePackages = "com.atguigu.gulimall.product.controller")
+public class GulimallExceptionControllerAdvice {
+
+    @ExceptionHandler(value = Exception.class) // 也可以返回ModelAndView
+    public R handleValidException(MethodArgumentNotValidException exception){
+
+        Map<String,String> map=new HashMap<>();
+        // 获取数据校验的错误结果
+        BindingResult bindingResult = exception.getBindingResult();
+        // 处理错误
+        bindingResult.getFieldErrors().forEach(fieldError -> {
+            String message = fieldError.getDefaultMessage();
+            String field = fieldError.getField();
+            map.put(field,message);
+        });
+
+        log.error("数据校验出现问题{},异常类型{}",exception.getMessage(),exception.getClass());
+
+        return R.error(BizCodeEnume.VAILD_EXCEPTION.getCode(),BizCodeEnume.VAILD_EXCEPTION.getMsg()).put("data",map);
+    }
+
+    @ExceptionHandler(value = Throwable.class)
+    public R handleException(Throwable throwable){
+
+        log.error("错误",throwable);
+        return R.error(BizCodeEnume.UNKNOW_EXCEPTION.getCode(), BizCodeEnume.UNKNOW_EXCEPTION.getMsg());
+    }
+}
+
--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,92 @@
 package com.atguigu.gulimall.product;
 
-import org.junit.jupiter.api.Test;
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.entity.CategoryEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import com.atguigu.gulimall.product.service.CategoryService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ * 1、引入oss-starter
+ * 2、配置key，endpoint相关信息即可
+ * 3、使用OSSClient 进行相关操作
+ */
+//@Slf4j
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallProductApplicationTests {
+public class GulimallProductApplicationTests {
+
+    @Autowired
+    BrandService brandService;
+
+//    @Autowired
+//    OSSClient ossClient;
+//    @Test
+//    public void testUpload() throws FileNotFoundException {
+////        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+////        String endpoint = "oss-cn-hangzhou.aliyuncs.com";
+////        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+////        String accessKeyId = "LTAI5t6gvDfMEFEVRRtyzviB";
+////        String accessKeySecret = "GCLiaTIviz2oUH35Eu4bTOQc8P9lDC";
+////
+////        // 创建OSSClient实例。
+////        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+//
+//        // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+//        InputStream inputStream = new FileInputStream("/Users/r1ff/Desktop/简历/证件照.JPG");
+//        // 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
+//        ossClient.putObject("gulimall-xixihaha", "证件照.JPG", inputStream);
+//
+//        // 关闭OSSClient。
+//        ossClient.shutdown();
+//        System.out.println("上传成功。。");
+//    }
+
+//    @Autowired
+//    CategoryService categoryService;
+//
+//    @Test
+//    public void testFindPath(){
+//        Long[] catelogPath = categoryService.findCatelogPath(225L);
+//        log.info("完整路径：{}",Arrays.asList(catelogPath));
+//    }
+
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+
+//        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setBrandId(1L);
+//        brandEntity.setDescript("华为");
+
+//
+//        brandEntity.setName("华为");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功....");
+
+//        brandService.updateById(brandEntity);
+
+
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+        list.forEach((item) -> {
+            System.out.println(item);
+        });
+
     }
 
 }

77788b4cfff92b06750a155010922ab1a6d3a56a
lufeifeile <790002348@qq.com>
2021-09-24 14:47:47 +0800

    商品服务-品牌管理-自定义校验注解

--- gulimall-common/src/main/java/com/atguigu/common/valid/ListValue.java
+++ gulimall-common/src/main/java/com/atguigu/common/valid/ListValue.java
@@ -10,16 +10,24 @@ import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+
+/*
+必须有3个属性：就是注解上可以写的东西
+        - message()错误信息
+        - groups()分组校验
+        - payload()自定义负载信息
+ */
 @Documented
-@Constraint(validatedBy = { ListValueConstraintValidator.class })
-@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Constraint(validatedBy = { ListValueConstraintValidator.class })// 关联校验器和校验注解
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })//哪些地方可以用
 @Retention(RUNTIME)
 public @interface ListValue {
-    String message() default "{com.atguigu.common.valid.ListValue.message}";
+    String message() default "{com.atguigu.common.valid.ListValue.message}";//消息从哪取得
 
-    Class<?>[] groups() default { };
+    Class<?>[] groups() default { };//默认分组校验
 
     Class<? extends Payload>[] payload() default { };
 
+    //数组，需要自己指定
     int[] vals() default { };
 }
--- gulimall-common/src/main/java/com/atguigu/common/valid/ListValueConstraintValidator.java
+++ gulimall-common/src/main/java/com/atguigu/common/valid/ListValueConstraintValidator.java
@@ -5,30 +5,32 @@ import javax.validation.ConstraintValidatorContext;
 import java.util.HashSet;
 import java.util.Set;
 
+//第一个泛型参数是所对应的校验注解类型，第二个是校验对象类型
 public class ListValueConstraintValidator implements ConstraintValidator<ListValue,Integer> {
-
+    // 用来存储指定的值
     private Set<Integer> set = new HashSet<>();
     //初始化方法
-    @Override
+    @Override// 通过重写initialize()方法，获得注解上的内容，放在集合里，完成指定实体参数需要指定值
     public void initialize(ListValue constraintAnnotation) {
 
-        int[] vals = constraintAnnotation.vals();
-        for (int val : vals) {
+        int[] vals = constraintAnnotation.vals();//写的注解是@ListValue(value={0,1})
+        for (int val : vals) {//遍历放入集合
             set.add(val);
         }
 
     }
 
-    //判断是否校验成功
 
     /**
      *
      * @param value 需要校验的值
      * @param context
      * @return
+     *真正的验证逻辑由`isValid`完成，如果传入形参的属性值在这个set里就返回true，否则返回false
      */
     @Override
     public boolean isValid(Integer value, ConstraintValidatorContext context) {
+
 
         return set.contains(value);
     }

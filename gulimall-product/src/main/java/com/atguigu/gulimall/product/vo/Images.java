e8dcc56a49901d7c27b60a2c7af7cddbb05722b3
lufeifeile <790002348@qq.com>
2021-10-04 16:24:37 +0800

    商品服务API-新增商品-商品新增vo抽取

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/Images.java
@@ -0,0 +1,30 @@
+/**
+  * Copyright 2021 bejson.com 
+  */
+package com.atguigu.gulimall.product.vo;
+
+/**
+ * Auto-generated: 2021-10-04 16:18:27
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class Images {
+
+    private String imgUrl;
+    private int defaultImg;
+    public void setImgUrl(String imgUrl) {
+         this.imgUrl = imgUrl;
+     }
+     public String getImgUrl() {
+         return imgUrl;
+     }
+
+    public void setDefaultImg(int defaultImg) {
+         this.defaultImg = defaultImg;
+     }
+     public int getDefaultImg() {
+         return defaultImg;
+     }
+
+}

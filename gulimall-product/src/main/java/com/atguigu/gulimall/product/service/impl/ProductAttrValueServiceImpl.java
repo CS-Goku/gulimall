da3aa701f7abcf49940a15f47dd17519ed5de092
lufeifeile <790002348@qq.com>
2021-10-05 16:16:53 +0800

    商品服务API-新增商品-保存spu基本信息

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/ProductAttrValueServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/ProductAttrValueServiceImpl.java
@@ -1,7 +1,15 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import com.atguigu.gulimall.product.entity.AttrEntity;
+import com.atguigu.gulimall.product.service.AttrService;
+import com.atguigu.gulimall.product.vo.BaseAttrs;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -15,6 +23,8 @@ import com.atguigu.gulimall.product.service.ProductAttrValueService;
 
 @Service("productAttrValueService")
 public class ProductAttrValueServiceImpl extends ServiceImpl<ProductAttrValueDao, ProductAttrValueEntity> implements ProductAttrValueService {
+    @Autowired
+    private AttrService attrService;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -24,6 +34,27 @@ public class ProductAttrValueServiceImpl extends ServiceImpl<ProductAttrValueDao
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public void saveProductAttrValue(Long id, List<BaseAttrs> baseAttrs) {
+        if (baseAttrs == null || baseAttrs.size() == 0){
+
+        }else {
+            List<ProductAttrValueEntity> collect = baseAttrs.stream().map(item -> {
+                ProductAttrValueEntity productAttrValueEntity = new ProductAttrValueEntity();
+                productAttrValueEntity.setSpuId(id);
+                productAttrValueEntity.setAttrId(item.getAttrId());
+                //需要另外查询属性名字，根据属性id
+                AttrEntity byId = attrService.getById(item.getAttrId());
+                productAttrValueEntity.setAttrName(byId.getAttrName());
+                productAttrValueEntity.setAttrValue(item.getAttrValues());
+                productAttrValueEntity.setQuickShow(item.getShowDesc());
+
+                return productAttrValueEntity;
+            }).collect(Collectors.toList());
+            this.saveBatch(collect);
+        }
     }
 
 }
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SpuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SpuInfoServiceImpl.java
@@ -1,8 +1,21 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import com.atguigu.gulimall.product.entity.SpuImagesEntity;
+import com.atguigu.gulimall.product.entity.SpuInfoDescEntity;
+import com.atguigu.gulimall.product.service.ProductAttrValueService;
+import com.atguigu.gulimall.product.service.SpuImagesService;
+import com.atguigu.gulimall.product.service.SpuInfoDescService;
+import com.atguigu.gulimall.product.vo.BaseAttrs;
 import com.atguigu.gulimall.product.vo.SpuSaveVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -17,6 +30,14 @@ import com.atguigu.gulimall.product.service.SpuInfoService;
 @Service("spuInfoService")
 public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> implements SpuInfoService {
 
+    @Autowired
+    private SpuInfoDescService spuInfoDescService;
+
+    @Autowired
+    private SpuImagesService spuImagesService;
+
+    @Autowired
+    private ProductAttrValueService productAttrValueService;
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<SpuInfoEntity> page = this.page(
@@ -30,12 +51,26 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
     @Override
     public void saveSpuInfo(SpuSaveVo vo) {
         //1、保存spu基本信息：pms_spu_info
+        SpuInfoEntity spuInfoEntity = new SpuInfoEntity();
+        BeanUtils.copyProperties(vo,spuInfoEntity);
+        spuInfoEntity.setCreateTime(new Date());
+        spuInfoEntity.setUpdateTime(new Date());
+        this.saveBaseSpuInfo(spuInfoEntity);
 
         //2、保存spu描述图片：pms_spu_info_desc
+        List<String> decript = vo.getDecript();
+        SpuInfoDescEntity spuInfoDescEntity = new SpuInfoDescEntity();
+        spuInfoDescEntity.setSpuId(spuInfoEntity.getId());
+        spuInfoDescEntity.setDecript(String.join(",",decript));
+        spuInfoDescService.saveSpuInfoDesc(spuInfoDescEntity);
 
-        //3、保存spu图片集：pms_spu_info_desc
+        //3、保存spu图片集：pms_spu_images
+        List<String> images = vo.getImages();
+        spuImagesService.saveImages(spuInfoEntity.getId(),images);
 
-        //4、保存spu规格参数：pms_spu_images
+        //4、保存spu规格参数：pms_product_attr_value
+        List<BaseAttrs> baseAttrs = vo.getBaseAttrs();
+        productAttrValueService.saveProductAttrValue(spuInfoEntity.getId(),baseAttrs);
 
         //5、保存spu积分信息：sms_spu_bounds
 
@@ -46,5 +81,12 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
         //5、3保存sku销售属性信息：pms_sku_sale_attr_value
         //5、4保存sku的优惠、满减信息：gulimall_sms->sms_sku_ladder\sms_sku_full_reduction\sms_member_price
     }
+
+    @Override
+    public void saveBaseSpuInfo(SpuInfoEntity spuInfoEntity) {
+        this.baseMapper.insert(spuInfoEntity);
+    }
+
+
 
 }

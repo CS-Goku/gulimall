9a415aab62437673968a169dc76e28c1f1b7b180
lufeifeile <790002348@qq.com>
2021-11-17 22:57:58 +0800

    P174页面跳转检索

---
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/controller/SearchController.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.search.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SearchController {
+
+    @GetMapping("/list.html")
+    public String listPage(){
+        return "list";
+    }
+}
